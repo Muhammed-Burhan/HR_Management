42fa528baafb5c233bfcbc2e8a3622ad50458447
Muhammed <87388234+Muhammed-Burhan@users.noreply.github.com>
2023-06-25 17:34:17 +0300

    Simple-fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
     $user = User::create([
         'name' => $request['name'],
         'email' => $request['email'],
-        'role'=>'super_admin',
+        'role'=>'admin',
         'password' => bcrypt($request['password']),
     ]);
 
